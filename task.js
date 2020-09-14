14f89eefa8c88c1cdfa932e7c320a80b518c1eae
Sagar B Belote <sagarbelotes@gmail.com>
2020-09-14 14:15:17 +0530

    comments added in code for better understanding while reading code

--- task.js
+++ task.js
@@ -1,68 +1,58 @@
 //Assignment submitted by Sagar B Belote.
-//email:sagarbelotes@gmail.com
-//contact number:8698928826
-//please look readme file because when i am trying to
+//email:sagarbelotes@gmail.com.
+//contact number:8698928826.
+//problem while understanding 3rd condition: when i am trying to code for add missing days with value vased on pre+next days value and then theirs mean is value of missing 
+//day that time i didnt get idea how you shown there a values because if i get idea you have taking mean of pre+next days value and taking mean of both but if we 
+//see in 3rd condition i found it is not logically correct.if i get to know how you are replacing thu=8 and fri=10 (this 2 are missing days in second input dictionary 
+//output).definitely i will write code for this condition too but i want a clear idea about it.
 
+//input dictionary 1:
 var D = {
-    '2020-01-01': 4, //wed
-    '2020-01-02': 4, //Thu 
-    '2020-01-03': 6, //fri
-    '2020-01-04': 8, //sat
-    '2020-01-05': 2, //sun
-    '2020-01-06': -6, //mon
-    '2020-01-07': 2, //tue
-    '2020-01-08': -2 //wed
+    '2020-01-01': 4,
+    '2020-01-02': 4,
+    '2020-01-03': 6,
+    '2020-01-04': 8,
+    '2020-01-05': 2,
+    '2020-01-06': -6,
+    '2020-01-07': 2,
+    '2020-01-08': -2
 }
-var output = {
-    'Mon': -6,
-    'Tue': 2,
-    'Wed': 2,
-    'Thu': 4,
-    'Fri': 6,
-    'Sat': 8,
-    'Sun': 2
-}
+//input dictionary 2:
+// var D = {
+//     '2020-01-01': 4,
+//     '2020-01-04': 8,
+//     '2020-01-05': 2,
+//     '2020-01-06': -6,
+//     '2020-01-07': 2
+// }
 
 var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-console.log("days:", days);
 solution(D, days);
-
+//below methods have nested functions every method in thos function have its own unique working.i have tried to use less methods to achieve result in this program.
 function solution(D, days) {
     var dictionary = {};
     var previousValue = 0;
     var inputDays = [];
     var isValidDictionary = true;
+    //loop below is making array of key(YYYY-MM-DD) after converting it in current week day based on key(YYYY-MM-DD),we can use it later for validation.
     for (var x in D) {
-
-        console.log("x:", x);
         var d = new Date(x);
         var dayinObject = d.getDay();
-        console.log("dayinObject:", dayinObject);
         var day = days[dayinObject];
-        console.log("day:", day);
         inputDays.push(day);
-
     }
 
     validateDictionary(D);
 
     function validateDictionary(D) {
-
+        //this function validates our input dictionary and return error(opens dialog box) when key or value cross the limits.
         Object.entries(D).forEach(([key, val]) => {
-            console.log("key of D:", key);
-            console.log("val of D:", val);
             var startDateLimit = new Date(0).getTime();; //JavaScript stores dates as number of milliseconds since January 01, 1970, 00:00:00 UTC (Universal Time Coordinated). 
-            console.log("val of startDateLimit:", startDateLimit);
             var EndDateLimit = new Date("2100-01-01").getTime();
-            console.log("val of EndDateLimit:", EndDateLimit);
             var currentDate = new Date(key).getTime();
-            console.log("val of currentDate:", currentDate);
             try {
                 if ((currentDate < startDateLimit) || (currentDate > EndDateLimit)) throw "invalid key found in input cant proceed,please provide input in dictionary between 1970-01-01 to 2100-01-01";
                 if ((val < -1000000) || (val > 1000000)) throw "invalid value found in input cant proceed,please provide key input dictionary between -1000000 to 1000000";
-                // else if (((val > -1000000) && (val < 1000000)) || ((currentDate < startDateLimit) && (currentDate < EndDateLimit))) {
-                //     isValidDictionary = true;
-                // }
             } catch (err) {
                 alert(err);
                 isValidDictionary = false;
@@ -70,19 +60,12 @@ function solution(D, days) {
         });
     }
 
-    console.log("inputDays:", inputDays);
-
     if ((inputDays.includes("Mon") === true) && (inputDays.includes("Sun") === true) && (isValidDictionary === true)) {
 
         Object.entries(D).forEach(([key, val]) => {
-            console.log("key:", key);
-            console.log("val:", val);
             var d = new Date(key);
             var dayFromDate = d.getDay();
-            console.log("dayFromDate:", dayFromDate);
-
             var day = days[dayFromDate];
-            console.log("day:", day);
             if (dictionary[day] !== undefined) {
                 console.log(day + " " + "is allready present here with value");
                 previousValue = dictionary[day];
@@ -90,11 +73,9 @@ function solution(D, days) {
                 updateValue(previousValue);
             }
             if (dictionary[day] == undefined) {
-                // previousValue = 0;
                 updateValue(previousValue);
-
             }
-
+            //if we have same day with diierent value then this function make addition of that days value and return sum and add it in dictionary with its key(e.g Mon:2+3 i.e Mon=5)
             function updateValue(previousValue) {
                 if (days.includes(day) === true) {
                     dictionary[day] = val + previousValue;
@@ -103,17 +84,27 @@ function solution(D, days) {
 
         });
     }
+    //below try catch code block is used to check if dictionary contains at least mon and sun,if not available it will throw error.
     try {
         if (inputDays.includes("Mon") !== true) throw "input dictionary will have at least Mon";
         if (inputDays.includes("Sun") !== true) throw "input dictionary will have at least Sun";
     } catch (err) {
         alert(err);
     }
-    // document.getElementById("demo").innerHTML = dictionary;
     console.log("dictionary:", dictionary); //expected dictionary output
+    var missingDays = [];
     if (dictionary !== undefined) {
-        console.log("true");
-        for (var missingValue = 0; missingValue < days; missingValue++) {}
+        //below code block is used to finf out missing days in input dictionary.
+        for (var missingValue = 0; missingValue < days.length; missingValue++) {
+            if ((dictionary.hasOwnProperty(days[missingValue]) == true)) {
+                continue;
+            } else {
+                missingDays.push(days[missingValue]);
+            }
+
+        }
 
     }
+    console.log("missing days from dictionary:", missingDays); //this array returns missing days from input dictionary.
+
 }
